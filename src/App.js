0979ca12604ce468e0b5da4eab59590c53b59f49
Jekyung Min <cwm7485@naver.com>
2022-08-17 08:13:15 +0900

    my-agora-states-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Discussions } from "./components/Discussions";
+import { Form } from "./components/Form";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [discussions, setDiscussions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/discussions')
+    .then(res => res.json())
+    .then(data => {
+      setDiscussions(data);
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My Agora States</h1>
+       <Form></Form>
+       <Discussions discussions={discussions}></Discussions>
+    </>
   );
 }
 
